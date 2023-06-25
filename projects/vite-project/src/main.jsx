1df0e0bc0f699423ee53350e2b6a1653f669e64a
Rezi <rezi.sirbiladze2@gmail.com>
2023-06-25 12:13:08 +0000

    ADD TicTacToe table logic

--- projects/vite-project/src/main.jsx
+++ projects/vite-project/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App_TwitterFollowCard } from './js/App_TwitterFollowCard.jsx'
+import { App_TicTacToe } from './js/App_TicTacToe.jsx'
 import './css/index.css'
+import './css/App_TicTacToe.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App_TwitterFollowCard />
+    <App_TicTacToe />
   </React.StrictMode>,
 )
